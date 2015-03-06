b29631644e704cc66597e26226fa355c53a169a8
J Irving <j@lollyshouse.ca>
2015-03-05 21:54:03 -0500

    Lighten comments/docstrings, make vars and defns the same colour

--- phoenix-dark-pink-theme.el
+++ phoenix-dark-pink-theme.el
@@ -44,11 +44,11 @@
  '(trailing-whitespace ((t (:background "#d1afdd" :underline nil :weight normal))))
 
  '(font-lock-builtin-face ((t (:weight normal :underline nil :foreground "#ddd"))))
- '(font-lock-comment-delimiter-face ((t (:weight normal :underline nil :foreground "#555" :inherit font-lock-comment-face))))
- '(font-lock-comment-face ((t (:foreground "grey50" :underline nil :slant italic :weight normal))))
+ '(font-lock-comment-delimiter-face ((t (:weight normal :underline nil :foreground "gray50" :inherit font-lock-comment-face))))
+ '(font-lock-comment-face ((t (:foreground "grey60" :underline nil :slant italic :weight normal))))
  '(font-lock-constant-face ((t (:foreground "#B294BB" :underline nil :weight normal))))
- '(font-lock-doc-face ((t (:weight normal :underline nil :foreground "#a582a3" :inherit (font-lock-string-face)))))
- '(font-lock-function-name-face ((t (:foreground "#EFEFEF" :underline nil :weight normal))))
+ '(font-lock-doc-face ((t (:weight normal :underline nil :foreground "gray60" :inherit (font-lock-string-face)))))
+ '(font-lock-function-name-face ((t (:foreground "#efefef" :underline nil :weight normal))))
  '(font-lock-keyword-face ((t (:foreground "#d1afdd" :underline nil :weight normal))))
  '(font-lock-negation-char-face ((t (:weight normal :underline nil :foreground "#cccccc"))))
  '(font-lock-preprocessor-face ((t (:weight normal :underline nil :foreground "#d1afdd" :inherit (font-lock-builtin-face)))))
@@ -57,6 +57,7 @@
  '(font-lock-string-face ((t (:foreground "#a582a3" :underline nil :weight normal))))
  '(font-lock-type-face ((t (:foreground "#e0e0e0" :underline nil :weight normal))))
  '(font-lock-variable-name-face ((t (:foreground "#787878" :underline nil :weight normal))))
+ '(font-lock-variable-name-face ((t (:foreground "#efefef" :underline nil :weight normal))))
  '(font-lock-warning-face ((t (:weight normal :underline nil :foreground "#c0af7f" :inherit (error)))))
 
  '(compilation-info ((t (:weight normal :foreground "#f0dfff"))))
