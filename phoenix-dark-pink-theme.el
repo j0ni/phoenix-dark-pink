4defbb76b00c1a29f060813898578152d6be623d
J Irving <j@lollyshouse.ca>
2017-07-29 10:03:20 -0400

    Add a license and bump the version
    Thanks to Jonas Bernoulli for the poke

--- phoenix-dark-pink-theme.el
+++ phoenix-dark-pink-theme.el
@@ -4,7 +4,20 @@
 
 ;; Author: J Irving <j@lollyshouse.ca>
 ;; URL: http://github.com/j0ni/phoenix-dark-pink
-;; Version: 1.4
+;; Version: 2
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;; Org-mode mods from Rikard Glans - https://github.com/darrik/phoenix-dark-pink
 
